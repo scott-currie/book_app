d7d7d710691dae600dccad86127d0196726b26e5
Scott Currie <vitriolik@gmail.com>
2018-10-30 09:43:55 -0700

    Added basics to server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 'use strict';
 const express = require('express');
+const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('err', err => console.log(err));
+
+const app = express();
+app.use(cors());
+
