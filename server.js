acd26146707ae99d60dcf794c049a1ce4ba4877e
Scott Currie <vitriolik@gmail.com>
2018-10-30 10:05:13 -0700

    Basic route added to server.js

--- server.js
+++ server.js
@@ -3,13 +3,24 @@ const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
+const ejs = require('ejs');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-client.on('err', err => console.log(err));
+
+
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.connect();
+// client.on('err', err => console.log(err));
 
 const app = express();
 app.use(cors());
+app.set('view engine', 'ejs');
+
+// Set routes
+app.get('/hello', function(req, res) {
+  res.render('pages/index');
+});
+
+app.listen(PORT,()  => console.log(`Listening on ${PORT}`));
 
