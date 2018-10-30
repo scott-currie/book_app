781f487b44b042410e45644d6a505a7a430796f6
Charles Clemens <cclemensjr@gmail.com>
2018-10-30 14:43:53 -0700

    Removed cors dependency due to not needed

--- server.js
+++ server.js
@@ -1,36 +1,26 @@
 'use strict';
+
+// Dependecy variables
 const express = require('express');
-const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
 const ejs = require('ejs');
 require('dotenv').config();
 const PORT = process.env.PORT;
-
-
-
-// const client = new pg.Client(process.env.DATABASE_URL);
-// client.connect();
-// client.on('err', err => console.log(err));
-
 const app = express();
-app.use(cors());
+
+// Middleware
 app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// Set routes
-// app.get('/hello', function(req, res) {
-//   res.render('pages/index');
-// });
-
+// CRUD actions
 app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
 app.post('/searches', postResults);
 
-app.listen(PORT,()  => console.log(`Listening on ${PORT}`));
 
 function Book(query) {
   this.title = (query.volumeInfo.title) ? query.volumeInfo.title : "Title not found";
@@ -41,20 +31,14 @@ function Book(query) {
 }
 
 function postResults(req, res) {
-  console.log('req.body', req.body);
   const query = req.body['book-search'][0];
   let _url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
-
-  // _url = (req.body['book-search'][1] === 'author') ?  '' : _url;
-
-
-  // console.log('_URL', _URL);
+  
   return superagent.get(_url)
-  .then((data) => {
-    console.log(data.body.items[0]);
-    const books = data.body.items.map(book => new Book(book));
-    res.render('pages/searches/show', {data: books});
+    .then((data) => {
+      const books = data.body.items.map(book => new Book(book));
+      res.render('pages/searches/show', {data: books});
   });
-  // res.render(_URL);
-
+  
 }
+app.listen(PORT,()  => console.log(`Listening on ${PORT}`));
