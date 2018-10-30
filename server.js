2938a9d639d621f031bc931197e13f1f9e8308e5
Scott Currie <vitriolik@gmail.com>
2018-10-30 13:32:42 -0700

    postResults fetches data correctly now

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/searches', postResults);
 app.listen(PORT,()  => console.log(`Listening on ${PORT}`));
 
 function Book(query) {
-  this.title = (query.title) ? query.title : "Title not found";
+  this.title = (query.volumeInfo.title) ? query.volumeInfo.title : "Title not found";
   this.author = (query.author) ? query.author: "Author not found";
   this.isbn = (query.isbn) ? query.isbn: "ISBN not found";
   this.image_url = (query.image_url) ? query.image_url: "Image not found";
@@ -41,11 +41,14 @@ function Book(query) {
 }
 
 function postResults(req, res) {
-  const _URL = `https://www.googleapis.com/books/v1/volumes?q=${req.query.title}`;
+  const query = req.body['book-search'][0];
+  const _URL = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+  // console.log('_URL', _URL);
   return superagent.get(_URL)
   .then((data) => {
-    console.log(data);
-    res.render('pages/searches/show', data);
+    // console.log(data.body.items[0].id);
+    const books = data.body.items.map(book => new Book(book));
+    res.render('pages/searches/show', {data: books});
   });
   // res.render(_URL);
 
